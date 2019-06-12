3ae1fbe787e701ca958ec513851a4e8395773f0a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-12 14:21:38 -0500

    WEBAUTH-III-CHALLENGE: Add dbconfig
    Added dbconfig and server.js

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
     client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: "./user.db3"
+      filename: "./data/user.db3"
     },
     migrations: {
       directory: "./data/migrations"
