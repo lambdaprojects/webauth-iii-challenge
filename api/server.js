75355fea457f65f19a99b5e9bfd4d8bf85dd318a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-12 16:08:24 -0500

    webauth-iii-challenge - Completed the authorization part
    The user can register and login and a token will be issued.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const server = express();
+const authRouter = require("../auth/auth-router.js");
 
 server.use(express.json());
 
+server.use("/api/auth", authRouter);
 server.get("/", (req, res) => {
   res.send("Its alive!!");
 });
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const UserHelper = require("../user/user-model.js");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
 
 const bcrypt = require("bcryptjs");
 
@@ -8,6 +11,40 @@ router.post("/register", async (req, res) => {
     let user = req.body;
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
-    //let addUser = await URLSe
-  } catch (error) {}
+    let addUser = await UserHelper.add(user);
+    res.status(201).json(addUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
+
+router.post("/login", async (req, res) => {
+  try {
+    let { username, password } = req.body;
+
+    let user = await UserHelper.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({ message: `Welcome! ${user.username}`, token });
+    } else {
+      res.status(401).json({ message: "Invalid Credentials" });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department
+  };
+
+  const options = {
+    expiresIn: "1d"
+  };
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
+module.exports = router;
