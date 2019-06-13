8b191f66c3cef39d05b90c52e0eaa8e69262a73f
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-13 16:24:14 -0500

    created react app
    Initial creation of react app

--- auth/checkDepartment.js
+++ auth/checkDepartment.js
@@ -1,6 +1,5 @@
 module.exports = function(department) {
   return function(req, res, next) {
-    console.log("::::::REQ USER DEPT ::::::" + req.user.department);
     if (req.user) {
       if (req.user.department && req.user.department.includes(department)) {
         next();
---
+++ client/src/components/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "../styles/App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className="App-card">
+          <div className="nav-bar">
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
+            <NavLink to="/register" className="nav-link">
+              Register
+            </NavLink>
+            <NavLink to="/logout" className="nav-link">
+              Logout
+            </NavLink>
+          </div>
+          <main>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/users" component={UserList} />
+          </main>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
