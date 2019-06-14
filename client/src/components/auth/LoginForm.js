4510cc24d230f070e64370a8277927a1908da66b
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-06-13 22:19:36 -0500

    Completed login and homepage
    completed the login and redirection to homepage with a list of users.

--- client/src/components/auth/LoginForm.js
+++ client/src/components/auth/LoginForm.js
@@ -1,6 +1,69 @@
 import React from "react";
+import "../../styles/App.css";
+import axios from "axios";
 
-function LoginForm() {
-  return <div>Login Form!!</div>;
+class LoginForm extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleChange = e => {
+    e.persist();
+    this.setState({
+      ...this.state,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const endpoint = "http://localhost:4000/api/auth/login";
+    axios
+      .post(endpoint, this.state)
+      .then(res => {
+        console.log(`RESPONSE DATA IS ${JSON.stringify(res.data.token)}`);
+        localStorage.setItem("jwt", res.data.token);
+        this.props.history.push("/users");
+      })
+      .catch(error => {
+        console.log(`ERROR IS ${error}`);
+      });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="register-card">
+          <div className="register-card-items">
+            <div>username:</div>
+            <div>
+              <input
+                type="text"
+                className="text-box"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="register-card-items">
+            <div>password:</div>
+            <div>
+              <input
+                type="password"
+                className="text-box"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+              />
+            </div>
+          </div>
+
+          <button value="submit">Login</button>
+        </div>
+      </form>
+    );
+  }
 }
 export default LoginForm;
--- client/src/components/user/UserList.js
+++ client/src/components/user/UserList.js
@@ -1,6 +1,41 @@
 import React from "react";
+import axios from "axios";
 
-function UserList() {
-  return <div>User List!!</div>;
+import "../auth/addInterceptors";
+import requireAuth from "../auth/requireAuth";
+
+class UserList extends React.Component {
+  state = {
+    users: []
+  };
+
+  render() {
+    return (
+      <>
+        <h2>Our Users</h2>
+
+        <ul>
+          {this.state.users.map(u => (
+            <li key={u.id}>{u.username}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+
+  componentDidMount() {
+    const endpoint = "/users";
+
+    axios
+      .get(endpoint)
+      .then(res => {
+        console.log("users", JSON.stringify(res.data));
+        this.setState(() => ({ users: res.data }));
+      })
+      .catch(({ response }) => {
+        console.error("users error", response);
+      });
+  }
 }
-export default UserList;
+
+export default requireAuth(UserList);
